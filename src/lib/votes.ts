809502eba1d741eb3c6de0510f1710feef687e1e
Andrew Price <andy.price@trilogy.com>
2026-02-25 15:51:04 -0700

    Revert "fix: auto-expand to Show All when changing size filter"
    This reverts commit 7c2f3e1595bcf41dc555c257cb246af507fe0cbe.

--- src/lib/votes.ts
+++ src/lib/votes.ts
@@ -206,7 +206,7 @@ export const useVotesStore = create<VotesState>((set, get) => ({
 
   setShowTopOnly: (showTopOnly) => set({ showTopOnly }),
 
-  setAltSizeFilter: (value) => set({ altSizeFilter: value, showTopOnly: false }),
+  setAltSizeFilter: (value) => set({ altSizeFilter: value }),
 
   loadCitySummaries: async () => {
     const { isAdmin, viewAsParent, releasedFilter, showUnscored } = get();
